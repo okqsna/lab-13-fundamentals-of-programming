5a60aa3d64299ddd27a3c6fae4af59609d1f5f9d
Oksana Moskviak <moskviak.pn@ucu.edu.ua>
2025-05-01 01:05:15 +0300

    done

--- delete node in bst/delete_node.py
+++ delete node in bst/delete_node.py
@@ -11,4 +11,25 @@ class Solution(object):
         :type key: int
         :rtype: Optional[TreeNode]
         """
-        
+        if root is None:
+            return None
+
+        if root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        elif root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val == key:
+            if not root.left and not root.right:
+                return None
+            elif not root.left and root.right:
+                return root.right
+            elif not root.right and root.left:
+                return root.left
+            else:
+                new_node = root.right
+                while new_node.left:
+                    new_node = new_node.left
+                root.val = new_node.val
+                root.right = self.deleteNode(root.right, new_node.val)
+
+        return root
